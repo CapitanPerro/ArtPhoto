ab42fabd06edf33bc0190d78cefe0b40461c089f
TheDiZkyt-DPV <57878199+thediskyt@users.noreply.github.com>
2021-07-16 19:59:36 +0200

    Add files via upload

---
+++ template-parts/header-styles.php
@@ -0,0 +1,113 @@
+<!-- header styles -->
+
+<link id="u-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i|Alex+Brush:400">
+<style>.u-header {
+  background-image: none;
+}
+.u-header .u-sheet-1 {
+  min-height: 206px;
+}
+.u-header .u-image-1 {
+  margin: 1px auto 0 0;
+}
+.u-header .u-logo-image-1 {
+  max-width: 249px;
+  max-height: 249px;
+}
+.u-header .u-menu-1 {
+  margin: -120px 0 0 auto;
+}
+.u-header .u-nav-1 {
+  font-size: 1rem;
+  letter-spacing: 0px;
+  font-weight: 700;
+  text-transform: uppercase;
+}
+.u-header .u-nav-2 {
+  box-shadow: 2px 2px 8px 0 rgba(128,128,128,1);
+}
+.u-header .u-nav-3 {
+  font-size: 1.25rem;
+}
+.u-header .u-nav-4 {
+  box-shadow: 2px 2px 8px 0 rgba(128,128,128,1);
+}
+.u-header .u-text-1 {
+  font-style: italic;
+  font-weight: 700;
+  font-family: "Alex Brush";
+  width: 335px;
+  margin: -58px auto 64px;
+}
+@media (max-width: 1199px) {
+  .u-header .u-image-1 {
+    width: auto;
+  }
+  .u-header .u-menu-1 {
+    width: auto;
+  }
+  .u-header .u-text-1 {
+    width: 335px;
+  }
+}
+@media (max-width: 991px) {
+  .u-header .u-sheet-1 {
+    min-height: 168px;
+  }
+  .u-header .u-image-1 {
+    margin-top: 23px;
+  }
+  .u-header .u-logo-image-1 {
+    max-width: 150px;
+    max-height: 150px;
+  }
+  .u-header .u-menu-1 {
+    margin-top: -78px;
+    margin-right: 23px;
+  }
+  .u-header .u-text-1 {
+    width: auto;
+    margin: -56px 209px 46px 176px;
+  }
+}
+@media (max-width: 767px) {
+  .u-header .u-sheet-1 {
+    min-height: 118px;
+  }
+  .u-header .u-image-1 {
+    margin-top: 10px;
+  }
+  .u-header .u-logo-image-1 {
+    max-width: 118px;
+    max-height: 118px;
+  }
+  .u-header .u-menu-1 {
+    margin-top: -65px;
+    margin-right: 19px;
+  }
+  .u-header .u-text-1 {
+    width: 338px;
+    margin: -49px auto 26px;
+  }
+}
+@media (max-width: 575px) {
+  .u-header .u-sheet-1 {
+    min-height: 80px;
+  }
+  .u-header .u-image-1 {
+    margin-top: 10px;
+    width: auto;
+  }
+  .u-header .u-logo-image-1 {
+    max-width: 59px;
+    max-height: 59px;
+  }
+  .u-header .u-menu-1 {
+    margin-top: -40px;
+    margin-right: 19px;
+  }
+  .u-header .u-text-1 {
+    width: auto;
+    margin: -42px 1px 20px;
+  }
+}</style>
